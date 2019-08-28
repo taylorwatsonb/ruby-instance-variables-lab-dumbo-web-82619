ef80712662d213d500d1fa6fcef07688f8202518
Taylor Watson <taylorwatsonb@gmail.com>
2019-08-28 11:18:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,15 @@
-class Dog 
-  def name = (dogs_name)
+class Dog
+ 
+  def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
-  
-  def name 
+ 
+  def name
     @this_dogs_name
-  end 
-end 
-
+  end
+end
+ 
 lassie = Dog.new
 lassie.name = "Lassie"
  
- puts lassie.name
+puts lassie.name
